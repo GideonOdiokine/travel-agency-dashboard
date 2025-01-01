194f019d1af9433379d294559dde6f46e0b42575
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-01-01 02:07:58 +0100

    clientLoader

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Outlet, redirect } from 'react-router'
 import { SidebarComponent } from '@syncfusion/ej2-react-navigations'
 import { MobileSidebar, NavItems } from 'components'
+import { account } from '~/appwrite/client';
+import { getExistingUser, storeUserData } from '~/appwrite/auth';
 
+
+export async function clientLoader() {
+    try {
+        const user = await account.get();
+
+        if (!user.$id) return redirect('/sign-in');
+
+        const existingUSer = await getExistingUser(user.$id)
+        if (existingUSer?.status === 'user') {
+            return redirect('/')
+        }
+
+        return existingUSer?.$id ? existingUSer : await storeUserData()
+    } catch (e) {
+        console.log('Error in clientLoader', e)
+        return redirect('/sign-in')
+    }
+}
 const AdminLayout = () => {
     return (
         <div className='admin-layout'>
