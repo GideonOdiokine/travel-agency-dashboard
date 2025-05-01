7ff472456e3afc7b28b0d738bdde5c2354dd3fa9
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-05-01 03:34:16 +0100

    sentry monitoring setup error fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { sentryReactRouter, type SentryReactRouterBuildOptions } from '@sentry/react-router';
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  ssr:{
-    noExternal:[/@syncfusion/]
-  }
-});
+const sentryConfig: SentryReactRouterBuildOptions = {
+    org: "jsmastery-wi",
+    project: "javascript-react",
+    // An auth token is required for uploading source maps.
+    authToken: "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"
+    // ...
+  };
+
+
+
+
+export default defineConfig(config => {
+    return {
+    plugins: [tailwindcss(),tsconfigPaths(), reactRouter(),sentryReactRouter(sentryConfig, config)],
+    sentryConfig, // Also pass the config here!
+    ssr:{
+        noExternal:[/@syncfusion/]
+      }
+    };
+
+  });
