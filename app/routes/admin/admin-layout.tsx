55beb33b5205bee0dd5f6dec6aa9df981c301a99
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-05-02 03:23:31 +0100

    format file

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,18 +1,18 @@
-import {Outlet, redirect} from "react-router";
-import {SidebarComponent} from "@syncfusion/ej2-react-navigations";
-import {MobileSidebar, NavItems} from "../../../components";
-import {account} from "~/appwrite/client";
-import {getExistingUser, storeUserData} from "~/appwrite/auth";
+import { Outlet, redirect } from "react-router";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import { MobileSidebar, NavItems } from "../../../components";
+import { account } from "~/appwrite/client";
+import { getExistingUser, storeUserData } from "~/appwrite/auth";
 
 export async function clientLoader() {
     try {
         const user = await account.get();
 
-        if(!user.$id) return redirect('/sign-in');
+        if (!user.$id) return redirect('/sign-in');
 
         const existingUser = await getExistingUser(user.$id);
 
-        if(existingUser?.status === 'user') {
+        if (existingUser?.status === 'user') {
             return redirect('/');
         }
 
